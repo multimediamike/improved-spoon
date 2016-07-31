7f8e0ad18cfdf33a7f37268706c0ab5214f5a1c2
Mike Melanson <mike@multimedia.cx>
2016-07-31 09:30:20 -0700

    Tsunami: refactor font packer
    The primary work is contained in a separate callable module.

--- tsunami/pack-tsunami-font.py
+++ tsunami/pack-tsunami-font.py
@@ -6,40 +6,28 @@ import sys
 
 FONT_HEADER_SIZE = 12
 
-def pack_font(base_name):
+def pack_font(font_header_bytes, base_dir):
     # load the sequence of PGM files
     i = 0
     pgm_files = []
     while 1:
-        filename = "%s-char-%03d.pgm" % (base_name, i)
+        filename = "%s/char-%03d.pgm" % (base_dir, i)
         if not os.path.exists(filename):
             break
         pgm_files.append(open(filename).read())
         i += 1
-    print "found %d char files" % (len(pgm_files))
 
     # get the bits/pixel from the firstd image
     bits_per_pixel = (int(pgm_files[0].splitlines()[2]) + 1) >> 1
 
-    # get the original header
-    old_font = open(base_name, "rb")
-    header = old_font.read(FONT_HEADER_SIZE)
-    old_font.close()
-
     # adjust the count in the header
-    (count32, count16, y_coord, x_coord, bpp) = struct.unpack("<IHHHH", header)
-    header = struct.pack("<IHHHH", len(pgm_files), len(pgm_files), y_coord, x_coord, bpp)
-
-    # create a new font file
-    new_filename = "new-%s" % (base_name)
-    new_font = open(new_filename, "wb")
-    new_font.write(header)
-
-    # create a placeholder offset for each character
-    new_font.write(struct.pack("<I", 0) * len(pgm_files))
+    (count32, count16, y_coord, x_coord, bpp) = struct.unpack("<IHHHH", font_header_bytes)
+    new_header = struct.pack("<IHHHH", len(pgm_files), len(pgm_files), y_coord, x_coord, bpp)
 
     # encode each character
     i = 0
+    char_offset = FONT_HEADER_SIZE + 4 * len(pgm_files)
+    char_list = ""
     for pgm in pgm_files:
         # parse the PGM header
         lines = pgm.splitlines()
@@ -47,12 +35,6 @@ def pack_font(base_name):
         (width, height) = lines[1].split()
         width = int(width)
         height = int(height)
-
-        # note the current offset, rewind and write it out
-        offset = new_font.tell()
-        new_font.seek(FONT_HEADER_SIZE + 4 * i, 0)
-        new_font.write(struct.pack("<I", offset))
-        new_font.seek(offset, 0)
 
         # create the 2-byte header
         char_header = (height << 5) | (width)
@@ -76,13 +58,31 @@ def pack_font(base_name):
             accumulator <<= (8 - bit_count)
             encoding += struct.pack("B", accumulator)
 
-        new_font.write(encoding)
+        # record the offset of this character
+        new_header += struct.pack("<I", char_offset)
+        char_offset += len(encoding)
+
+        # append the new char encoding to the list
+        char_list += encoding
         i += 1
-    new_font.close()
+
+    return new_header + char_list
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "USAGE: invoke with base font filename"
+    if len(sys.argv) < 4:
+        print "USAGE: pack_tsunami_font <original font> <new font> <character directory>"
         sys.exit(1)
 
-    pack_font(sys.argv[1])
+    # read the original font header
+    font_header_bytes = open(sys.argv[1], "rb").read(FONT_HEADER_SIZE)
+
+    # make sure the directory is there
+    if not os.path.exists(sys.argv[3]):
+        print "'%s' is not a directory" % (sys.argv[3])
+        sys.exit(1)
+
+    # pack the font
+    font_data = pack_font(font_header_bytes, sys.argv[3])
+
+    # write the new font
+    open(sys.argv[2], "wb").write(font_data)
