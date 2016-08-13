91933151bceb786d3250a9afd4a0ac38f10836c8
Mike Melanson <mike@multimedia.cx>
2016-08-12 21:01:08 -0700

    Tsunami RLB: Unpack the strip resource
    This change unpacks the strip resources from the Return To Ringworld RLB
    file into different text files suitable for translation.

--- tsunami/unpack-rlb-file.py
+++ tsunami/unpack-rlb-file.py
@@ -10,8 +10,22 @@ import unpack_tsunami_font
 BLOCK_HEADER_SIZE = 6
 BLOCK_SIGNATURE = "TMI-"
 ENTRY_SIZE = 12
+RESOURCE_TYPE_STRIP = 1
 RESOURCE_TYPE_MESSAGE = 6
 RESOURCE_TYPE_FONT = 7
+STRIP_STRUCT_SIZE = 126
+
+HANDLED_RESOURCE_TYPES = [ RESOURCE_TYPE_STRIP, RESOURCE_TYPE_MESSAGE, RESOURCE_TYPE_FONT ]
+
+def read_cstr(block, offset):
+    cstr = ""
+    while 1:
+        c = block[offset]
+        if c == '\0':
+            return cstr
+        else:
+            cstr += c
+            offset += 1
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -60,40 +74,44 @@ if __name__ == "__main__":
             print "  **** block does not have 'TMI-' signature"
             sys.exit(1)
 
-        if block_type == RESOURCE_TYPE_FONT or block_type == RESOURCE_TYPE_MESSAGE:
+        if block_type in HANDLED_RESOURCE_TYPES:
             #print "resource #%d, type %d @ offset 0x%X" % (res_num, block_type, block_offset)
             (resource_type, entry_count) = struct.unpack("BB", header[4:6])
-            if entry_count > 1:
-                print "don't know how to handle font or message resources with more than one entry"
-                sys.exit(1)
             if resource_type != block_type:
                 print "resource type mismatch (index says %d, block says %d)" % (block_type, resource_type)
 
-            # read the single resource entry
-            entry = rlb.read(ENTRY_SIZE)
-            (resource_id, comp_size, uncomp_size, high, res_type, offset) = \
-                struct.unpack("<HHHBBI", entry)
-            #print "  id %d, type %d, sizes: (%d, %d, %d), offset = 0x%X" % (resource_id, res_type, comp_size, uncomp_size, high, offset)
+            # read all of the entries
+            entries = []
+            for j in xrange(entry_count):
+                entry_bytes = rlb.read(ENTRY_SIZE)
+                (entry_id, comp_size, uncomp_size, high, res_type, offset) = \
+                    struct.unpack("<HHHBBI", entry_bytes)
+                #print "  id %d, type %d, sizes: (%d, %d, %d), offset = 0x%X" % (entry_id, res_type, comp_size, uncomp_size, high, offset)
 
-            if comp_size != uncomp_size:
-                print "Decompression not implemented yet"
-                sys.exit(1)
+                if comp_size != uncomp_size:
+                    print "Decompression not implemented yet"
+                    sys.exit(1)
 
-            rlb.seek(block_offset + offset, 0)
-            payload = rlb.read(uncomp_size)
+                cur_pos = rlb.tell()
+                rlb.seek(block_offset + offset, 0)
+                payload = rlb.read(uncomp_size)
+                entries.append({ 'payload': payload, 'size': uncomp_size })
+                rlb.seek(cur_pos, os.SEEK_SET)
 
             if block_type == RESOURCE_TYPE_FONT:
                 filename = "%s/resource-font-%02d.dat" % (resource_dir, res_num)
-                print " dumping font %d -> '%s'" % (res_num, filename)
-                open(filename, "wb").write(payload)
+                print "dumping font %d -> '%s'" % (res_num, filename)
+                open(filename, "wb").write(entries[0]['payload'])
                 font_dir = "%s/resource-font-%02d" % (resource_dir, res_num)
                 if not os.path.exists(font_dir):
                     os.makedirs(font_dir)
                 unpack_tsunami_font.unpack_font(open(filename, "rb"), font_dir)
             elif block_type == RESOURCE_TYPE_MESSAGE:
+                payload = entries[0]['payload']
+                size = entries[0]['size']
                 message_list = []
                 message = ""
-                for j in xrange(uncomp_size):
+                for j in xrange(size):
                     if payload[j] == '\0':
                         pair = {}
                         pair['English'] = message
@@ -109,5 +127,36 @@ if __name__ == "__main__":
                 message_file = "%s/messages-%04d.json.txt" % (resource_dir, res_num)
                 print "dumping message resource %d -> '%s'" % (res_num, message_file)
                 open(message_file, "w").write(json.dumps(message_list, indent=2))
+            elif block_type == RESOURCE_TYPE_STRIP:
+                struct_count = entries[0]['size'] /  STRIP_STRUCT_SIZE
+                records = entries[0]['payload']
+                messages = entries[1]['payload']
+                object_list = []
+                for si in range(0, struct_count*STRIP_STRUCT_SIZE, STRIP_STRUCT_SIZE):
+                    record = records[si:si+STRIP_STRUCT_SIZE]
+                    byte_string = ""
+                    for c in record:
+                        byte_string += "%02X " % (struct.unpack("B", c)[0])
+                    byte_string = byte_string.strip()
+                    record_id = struct.unpack("<H", record[12:14])[0]
+                    message_list = []
+                    for ri in range(44, 44 + (8 * 10), 10):
+                        (str_id, offset) = struct.unpack("<HH", record[ri:ri+4])
+                        if str_id > 0:
+                            message = read_cstr(messages, offset)
+                            pair = {}
+                            pair['id'] = str_id
+                            pair['English'] = message
+                            pair['Spanish'] = message
+                            message_list.append(pair)
+                    speaker_offset = struct.unpack("<H", record[124:126])[0]
+                    speaker = read_cstr(messages, speaker_offset)
+                    object_list.append({ 'speaker': speaker, 'strings': message_list })
+
+                    strip_file = "%s/strip-%04d.json.txt" % (resource_dir, res_num)
+                    print "dumping strip resource %d -> '%s'" % (res_num, strip_file)
+                    open(strip_file, "w").write(json.dumps(object_list, indent=2))
+
+        # advance to next index entry
         i += 6
 
