4aad531cb42d521a86a9d2dcc0448affc6962b7d
Mike Melanson <mike@multimedia.cx>
2018-09-14 09:35:37 -0700

    New tool for printing SCUMM tree hierarchy

---
+++ scumm/print-tree.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# print-tree.py
+#  by Mike Melanson (mike -at- multimedia.cx)
+#
+# Call the scummtools module to parse a SCUMM resource file and print
+# its hierarchical tree to standard output.
+
+import os
+import sys
+
+import scummtools
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "USAGE: print-tree.py scumm-resource-file"
+        sys.exit(1)
+
+    scummData = open(sys.argv[1], "rb").read()
+    scummTree = scummtools.HETree(None, isRoot=True)
+    scummTree.parseBlob(scummData)
+    scummTree.printTree()
